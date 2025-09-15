50de9c6733a9f2bb815f70e384ecb60353938f9d
2022101202010120 <marcos2003mlr@gmail.com>
2025-09-15 10:47:06 -0300

    Iniciando desenvolvimento da tela login

---
+++ lib/screens/login_screen/Login_Screen.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class login_Screen extends StatefulWidget {
+  const login_Screen({super.key});
+
+  @override
+  State<login_Screen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<login_Screen> {
+   bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF023047),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
