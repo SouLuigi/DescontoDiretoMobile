76c935649c7967063eed00418c28d29bb2e6846f
2022101202010120 <marcos2003mlr@gmail.com>
2025-09-18 10:38:43 -0300

    feat: personalização da fonte dos buttons

--- lib/screens/login_screen/components/login_button.dart
+++ lib/screens/login_screen/components/login_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginButton extends StatelessWidget{
+class LoginButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
   final Color color;
@@ -13,25 +14,23 @@ class LoginButton extends StatelessWidget{
     required this.onPressed,
     this.color = const Color(0xFF023047),
     this.textColor = const Color(0xFFFFB703),
-}): super(key: key);
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         backgroundColor: color,
-        padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15)
-        )
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       ),
-        onPressed: onPressed,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: textColor,
-            fontSize: 16,
-          ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: GoogleFonts.kaiseiDecol(
+          textStyle: TextStyle(color: textColor, fontSize: 16),
         ),
+      ),
     );
   }
 }
